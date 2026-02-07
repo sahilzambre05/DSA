de5b7031db79fb8bc2ccc3470939c917aaae162d
Sahil Zambre <sahildinesh.zambre2023@vitstudent.ac.in>
2026-02-07 15:44:38 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (67.29%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int fib(int n) {
+        int qb[] = new int[n+1];
+        int fib = count(n,qb);
+        return fib;
+    }
+
+    public int count(int n,int[] qb){
+        if(n==0||n==1){
+            return n;
+        }
+        if(qb[n]!=0){
+            return qb[n];
+        }
+        
+        int ans = count(n-1,qb)+count(n-2,qb);
+        qb[n]=ans;
+        
+        return ans;
+        
+    }
+}
