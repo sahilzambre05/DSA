f3fb678b35fb2bc1121b78fe6f5f5397d57c2035
Sahil Zambre <sahildinesh.zambre2023@vitstudent.ac.in>
2026-02-13 05:54:31 +0530

    Time: 1 ms (85.19%) | Memory: 43.6 MB (16.10%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -4,24 +4,41 @@ public class Solution {
             return 0;
         }
 
+        // int n = s.length();
+        // int[] dp = new int[n + 1];
+        // dp[0] = 1;
+        // dp[1] = 1;
+
+        // for (int i = 2; i <= n; ++i) {
+        //     int oneDigit = s.charAt(i - 1) - '0';
+        //     int twoDigits = Integer.parseInt(s.substring(i - 2, i));
+
+        //     if (oneDigit != 0) {
+        //         dp[i] += dp[i - 1];
+        //     }
+
+        //     if (10 <= twoDigits && twoDigits <= 26) {
+        //         dp[i] += dp[i - 2];
+        //     }
+        // }
+
+        // return dp[n];
+
         int n = s.length();
-        int[] dp = new int[n + 1];
-        dp[0] = 1;
-        dp[1] = 1;
-
-        for (int i = 2; i <= n; ++i) {
-            int oneDigit = s.charAt(i - 1) - '0';
-            int twoDigits = Integer.parseInt(s.substring(i - 2, i));
-
-            if (oneDigit != 0) {
-                dp[i] += dp[i - 1];
+        int[] dp = new int[n+1];
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;++i){
+            int one = s.charAt(i-1)-'0';
+            int two = Integer.parseInt(s.substring(i-2,i));
+            if(one!=0){
+                dp[i]+=dp[i-1];
             }
 
-            if (10 <= twoDigits && twoDigits <= 26) {
-                dp[i] += dp[i - 2];
+            if(two>=10 && two<=26){
+                dp[i]+=dp[i-2];
             }
         }
-
         return dp[n];
     }
 }
