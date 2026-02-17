de8f95871fc1a2efac5b6776bc26ddc659ce3420
Sahil Zambre <sahildinesh.zambre2023@vitstudent.ac.in>
2026-02-17 17:52:22 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (36.31%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int rob(int[] nums) {
+        int oinc=nums[0];
+        int oexc=0;
+        int ninc=0;
+        int nexc=0;
+        for(int i=1;i<nums.length;i++){
+            ninc = nums[i]+oexc;
+            nexc = Math.max(oinc,oexc);
+            oexc = nexc;
+            oinc = ninc;
+        }
+
+        return Math.max(oexc,oinc);
+    }
+}
