c553ecf40bdd2ebedb42ed6d59522f9f8ec31114
Sahil Zambre <sahildinesh.zambre2023@vitstudent.ac.in>
2026-03-01 18:27:03 +0530

    Time: 5 ms (5.43%) | Memory: 46.4 MB (5.21%) - LeetSync

--- 4229-trim-trailing-vowels/trim-trailing-vowels.java
+++ 4229-trim-trailing-vowels/trim-trailing-vowels.java
@@ -1,18 +1,44 @@
-class Solution {
-    public String trimTrailingVowels(String s) {
-        int i=s.length()-1;
-        while(i>=0 && isVowel(s.charAt(i))){
-            i--;
-        }
-        if(i<0) return "";
+// class Solution {
+//     public String trimTrailingVowels(String s) {
+//         int i=s.length()-1;
+//         while(i>=0 && isVowel(s.charAt(i))){
+//             i--;
+//         }
+//         if(i<0) return "";
 
-        return s.substring(0,i+1);
-    }
+//         return s.substring(0,i+1);
+//     }
 
-    public boolean isVowel(char a){
-        if(a=='a' || a=='e' || a=='i' || a=='o' || a=='u'){
-            return true;
+//     public boolean isVowel(char a){
+//         if(a=='a' || a=='e' || a=='i' || a=='o' || a=='u'){
+//             return true;
+//         }
+//         return false;
+//     }
+// }
+
+class Solution{
+    public String trimTrailingVowels(String s){
+        Stack<Character> stack = new Stack<>();
+        StringBuilder ans =  new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            stack.push(s.charAt(i));
         }
-        return false;
+
+        
+
+        while(stack.peek()=='a' || stack.peek()=='e' ||stack.peek()=='i' ||stack.peek()=='o' ||stack.peek()=='u'){
+            stack.pop();
+            if(stack.isEmpty()){
+                return "";
+            }
+        }
+
+        while(!stack.isEmpty()){
+            ans.append(stack.pop());
+        }
+
+        String res = ans.reverse().toString();
+        return res;
     }
 }
