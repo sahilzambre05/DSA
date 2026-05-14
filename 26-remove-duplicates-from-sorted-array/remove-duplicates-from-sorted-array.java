22867b993d24b82ee2ad8c0bcca7cc717cd03944
Sahil Zambre <sahildinesh.zambre2023@vitstudent.ac.in>
2026-05-14 11:08:56 +0530

    Time: 1 ms (76.55%) | Memory: 46.5 MB (90.52%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        
+        int i=0,j=0;
+        while(j<nums.length){
+            if(nums[i]!=nums[j]){
+                int temp = nums[i+1];
+                nums[i+1] = nums[j];
+                nums[j] = temp;
+                i++;
+            }
+            j++;
+        }
+        return i+1; 
+    }
+}
