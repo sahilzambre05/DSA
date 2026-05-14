ece4bb8ee797a34f00dde6bece3cd6a3462d0363
Sahil Zambre <sahildinesh.zambre2023@vitstudent.ac.in>
2026-05-14 08:23:54 +0530

    Time: 2 ms (91.70%) | Memory: 48 MB (19.34%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i=0,j=0;
+        while(j<nums.length){
+            if(nums[j]!=0){
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                i++;
+            }
+            j++;
+        }
+    }
+    
+}
