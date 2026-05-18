4c468f00550425f1d6c0972fc07cdf1b13fc7f11
Sahil Zambre <sahildinesh.zambre2023@vitstudent.ac.in>
2026-05-18 14:36:22 +0530

    Time: 11 ms (32.83%) | Memory: 44.3 MB (14.35%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
@@ -1,21 +1,32 @@
 class Solution {
     public int maxNumberOfBalloons(String text) {
-        int b=0,a=0,l=0,o=0,n=0;
-        for(int i=0;i<text.length();i++){
-            if(text.charAt(i)=='b'){
-                b++;
-            }else if(text.charAt(i)=='a'){
-                a++;
-            }else if(text.charAt(i)=='l'){
-                l++;
-            }else if(text.charAt(i)=='o'){
-                o++;
-            }else if(text.charAt(i)=='n'){
-                n++;
-            }
+        // int b=0,a=0,l=0,o=0,n=0;
+        // for(int i=0;i<text.length();i++){
+        //     if(text.charAt(i)=='b'){
+        //         b++;
+        //     }else if(text.charAt(i)=='a'){
+        //         a++;
+        //     }else if(text.charAt(i)=='l'){
+        //         l++;
+        //     }else if(text.charAt(i)=='o'){
+        //         o++;
+        //     }else if(text.charAt(i)=='n'){
+        //         n++;
+        //     }
+        // }
+        // l/=2;o/=2;
+        // int ans = Math.min(b,Math.min(a,Math.min(l,Math.min(o,n))));
+        // return ans;
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(char t : text.toCharArray()){
+            map.put(t,map.getOrDefault(t,0)+1);
         }
-        l/=2;o/=2;
-        int ans = Math.min(b,Math.min(a,Math.min(l,Math.min(o,n))));
+        int ans = Integer.MAX_VALUE;
+        ans = Math.min(ans,map.getOrDefault('b',0));
+        ans = Math.min(ans,map.getOrDefault('a',0));
+        ans = Math.min(ans,map.getOrDefault('n',0));
+        ans = Math.min(ans,(map.getOrDefault('l',0)/2));
+        ans = Math.min(ans,(map.getOrDefault('o',0)/2));
         return ans;
     }
 }
