68aa9955a0ebdcc787c8ce3e1fd69909d7964f5a
Sahil Zambre <sahildinesh.zambre2023@vitstudent.ac.in>
2026-05-18 09:49:40 +0530

    Time: 5 ms (4.62%) | Memory: 43.5 MB (5.18%) - LeetSync

---
+++ 190-reverse-bits/reverse-bits.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int reverseBits(int n) {
+        String s = Integer.toBinaryString(n);
+        while(s.length()<32){
+            s = "0" + s;
+        }
+        String rev = new StringBuilder(s).reverse().toString();
+        long ans = Long.parseLong(rev,2);
+        return (int) ans;
+    }
+}
