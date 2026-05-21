7cf2e81382297e403c5589ee3ac1f542ed025409
Sahil Zambre <sahildinesh.zambre2023@vitstudent.ac.in>
2026-05-21 13:44:23 +0530

    Time: 7 ms (17.49%) | Memory: 44.8 MB (8.31%) - LeetSync

---
+++ 43-multiply-strings/multiply-strings.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String multiply(String num1, String num2) {
+        char[] s1 = reverse(num1);
+        char[] s2 = reverse(num2);
+        int[] res = new int[s1.length+s2.length];
+        for(int i=0;i<s1.length;i++){
+            int carry=0;
+            int start=i;
+            for(int j=0;j<s2.length;j++){
+                int n1 = s1[i]-'0';
+                int n2 = s2[j]-'0';
+                int sum = (n1*n2)+res[start]+carry;
+                int base = sum%10;
+                carry = sum/10;
+                res[start++] = base;
+            }
+            if(carry!=0) res[start] = carry;
+        }
+
+        int i = res.length-1;
+        while(i>0 && res[i]==0) i--;
+        String ans = "";
+        while(i>=0){
+            ans+=res[i--];
+        }
+        return ans;
+
+    }
+
+    public char[] reverse(String num1){
+        String rev = new StringBuilder(num1).reverse().toString();
+        char[] res = rev.toCharArray();
+        return res;
+    }
+}
