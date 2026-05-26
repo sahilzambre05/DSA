7b5a3f1d3653f35bbe18e4d11a44b478da907a3b
Sahil Zambre <sahildinesh.zambre2023@vitstudent.ac.in>
2026-05-26 19:01:25 +0530

    Time: 78 ms (15.51%) | Memory: 106.7 MB (67.27%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> stack = new Stack<>();
+        int[] res = new int[temperatures.length];
+        for(int i=temperatures.length-1;i>=0;i--){
+            while(!stack.isEmpty() && temperatures[i]>=temperatures[stack.peek()]){
+                stack.pop();
+            }
+
+            if(!stack.isEmpty()){
+                res[i] = stack.peek()-i;
+            }
+
+            stack.push(i);
+        }
+        return res;
+    }
+}
