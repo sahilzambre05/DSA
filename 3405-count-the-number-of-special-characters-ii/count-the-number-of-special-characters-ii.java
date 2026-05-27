537f715e8d94a5d0bc1f0f261a347f0c56f3918a
Sahil Zambre <sahildinesh.zambre2023@vitstudent.ac.in>
2026-05-27 10:21:11 +0530

    Time: 8 ms (97.11%) | Memory: 48 MB (64.88%) - LeetSync

---
+++ 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int numberOfSpecialChars(String word) {
+        int[] lower = new int[26];
+        int[] upper = new int[26];
+
+        Arrays.fill(lower,-1);
+        Arrays.fill(upper,-1);
+
+        for(int i=0;i<word.length();i++){
+            char ch = word.charAt(i);
+            if(ch>='a' && ch<='z'){
+                lower[ch-'a'] = i;
+            }else{
+                if(upper[ch-'A']==-1){
+                    upper[ch-'A'] = i;
+                }
+            }
+        }
+
+        int cnt=0;
+        for(int i=0;i<26;i++){
+            if(lower[i]!=-1 && upper[i]!=-1 && lower[i]<upper[i])
+                cnt++;
+        }
+        return cnt;
+
+    }
+}
