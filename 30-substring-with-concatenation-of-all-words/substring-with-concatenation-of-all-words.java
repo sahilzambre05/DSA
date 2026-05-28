313e1341ea06a6b67a8c355094da16a737992ab1
Sahil Zambre <sahildinesh.zambre2023@vitstudent.ac.in>
2026-05-28 22:30:46 +0530

    Time: 11 ms (98.67%) | Memory: 47.4 MB (20.88%) - LeetSync

---
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.java
@@ -0,0 +1,76 @@
+class Solution {
+    public List<Integer> findSubstring(String s, String[] words) {
+
+        List<Integer> res = new ArrayList<>();
+
+        if (s == null || s.length() == 0 || words.length == 0) {
+            return res;
+        }
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (String word : words) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+
+        int wordLen = words[0].length();
+        int totalWords = words.length;
+
+        for (int i = 0; i < wordLen; i++) {
+
+            int left = i;
+            int count = 0;
+
+            HashMap<String, Integer> seen = new HashMap<>();
+
+            for (int right = i; right + wordLen <= s.length(); right += wordLen) {
+
+                String word = s.substring(right, right + wordLen);
+
+                // Valid word
+                if (map.containsKey(word)) {
+
+                    seen.put(word, seen.getOrDefault(word, 0) + 1);
+                    count++;
+
+            
+                    while (seen.get(word) > map.get(word)) {
+
+                        String leftWord =
+                                s.substring(left, left + wordLen);
+
+                        seen.put(leftWord,
+                                seen.get(leftWord) - 1);
+
+                        left += wordLen;
+                        count--;
+                    }
+
+            
+                    if (count == totalWords) {
+
+                        res.add(left);
+
+                        String leftWord =
+                                s.substring(left, left + wordLen);
+
+                        seen.put(leftWord,
+                                seen.get(leftWord) - 1);
+
+                        left += wordLen;
+                        count--;
+                    }
+
+                } else {
+
+                
+                    seen.clear();
+                    count = 0;
+                    left = right + wordLen;
+                }
+            }
+        }
+
+        return res;
+    }
+}
