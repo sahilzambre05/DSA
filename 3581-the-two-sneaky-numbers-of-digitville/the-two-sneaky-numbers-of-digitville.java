c5b0aef2cb13488cdae2a8fdf11fdad06ef0ad40
Sahil Zambre <sahildinesh.zambre2023@vitstudent.ac.in>
2026-05-28 18:14:26 +0530

    Time: 5 ms (15.51%) | Memory: 46.5 MB (36.60%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.java
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.java
@@ -1,17 +1,27 @@
 class Solution {
     public int[] getSneakyNumbers(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int num : nums){
-            map.put(num,map.getOrDefault(num,0)+1);
-        }
+        // HashMap<Integer,Integer> map = new HashMap<>();
+        // for(int num : nums){
+        //     map.put(num,map.getOrDefault(num,0)+1);
+        // }
 
+        // int k=0;
+        // int res[] = new int[2];
+
+        // for(int i=0;i<nums.length;i++){
+        //     if(map.get(nums[i])==2){
+        //         res[k++] = nums[i];
+        //         map.put(nums[i],0);
+        //     }
+        // }
+        // return res;
+
+        Arrays.sort(nums);
         int k=0;
         int res[] = new int[2];
-
-        for(int i=0;i<nums.length;i++){
-            if(map.get(nums[i])==2){
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]){
                 res[k++] = nums[i];
-                map.put(nums[i],0);
             }
         }
         return res;
