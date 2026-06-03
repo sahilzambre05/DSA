a94087643243c5664f48f7da1607aaee83ed9cf4
Sahil Zambre <sahildinesh.zambre2023@vitstudent.ac.in>
2026-06-03 20:19:50 +0530

    Time: 14 ms (38.81%) | Memory: 47.3 MB (97.74%) - LeetSync

---
+++ 4131-minimum-flips-to-make-binary-string-coherent/minimum-flips-to-make-binary-string-coherent.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minFlips(String s) {
+        int one=0;
+        for(char a : s.toCharArray()){
+            if(a=='1'){
+                one++;
+            }
+        }
+        int zero = s.length()-one;
+        int ans = s.length();
+        ans = Math.min(ans,one);
+        ans = Math.min(ans,zero);
+        ans = Math.min(ans,Math.abs(one-1));
+        if(s.length()>1){
+            int cost=0;
+            if(s.charAt(0)=='0') cost++;
+            if(s.charAt(s.length()-1)=='0') cost++;
+            for(int i=1;i<s.length()-1;i++){
+                if(s.charAt(i)=='1') cost++;
+            }
+            ans = Math.min(ans,cost);
+        }
+
+        return ans;
+
+    }
+}
