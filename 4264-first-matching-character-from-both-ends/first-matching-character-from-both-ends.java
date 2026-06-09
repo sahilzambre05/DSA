709dd5025333159de8192facda0c874e5cf110fa
Sahil Zambre <sahildinesh.zambre2023@vitstudent.ac.in>
2026-06-09 09:53:18 +0530

    Time: 1 ms (35.63%) | Memory: 43.3 MB (48.29%) - LeetSync

---
+++ 4264-first-matching-character-from-both-ends/first-matching-character-from-both-ends.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int firstMatchingIndex(String s) {
+        // int cnt=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)==s.charAt(s.length()-i-1)){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
