d55eefa4f687a073a02e58f3db7b35d957c15e36
Sahil Zambre <sahildinesh.zambre2023@vitstudent.ac.in>
2026-06-09 19:51:15 +0530

    Time: 2 ms (43.01%) | Memory: 46.3 MB (44.56%) - LeetSync

---
+++ 4255-minimum-absolute-difference-between-two-values/minimum-absolute-difference-between-two-values.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minAbsoluteDifference(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        for(int i=0;i<nums.length;i++){
+            for(int j=0;j<nums.length;j++){
+                if(nums[i]==1 && nums[j]==2){
+                    min = Math.min(min,Math.abs(i-j));
+                }
+            }
+        }
+        return (min==Integer.MAX_VALUE)?-1:min;
+    }
+}
