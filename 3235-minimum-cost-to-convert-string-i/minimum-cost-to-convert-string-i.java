9a1ed3a04a15889f42828d6d8c33d1d33c3094bb
Sahil Zambre <sahildinesh.zambre2023@vitstudent.ac.in>
2026-07-13 09:44:18 +0530

    Time: 13 ms (95.05%) | Memory: 48.2 MB (26.73%) - LeetSync

--- 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.java
+++ 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.java
@@ -1,41 +1,44 @@
 class Solution {
     public long minimumCost(String source, String target, char[] original, char[] changed, int[] cost) {
-        int[][] dist = new int [26][26];
+        int[][] path = new int[26][26];
         for(int i=0;i<26;i++){
-            Arrays.fill(dist[i],Integer.MAX_VALUE);
-            dist[i][i]=0;
+            for(int j=0;j<26;j++){
+                path[i][j] = Integer.MAX_VALUE;
+                path[i][i] = 0;
+            }
         }
 
         for(int i=0;i<cost.length;i++){
-            int start = original[i]-'a';
-            int end = changed[i]-'a';
-            dist[start][end] = Math.min(dist[start][end],cost[i]);
+            int u = original[i] - 'a';
+            int v = changed[i] - 'a';
+            int w = cost[i];
+            if(path[u][v]>w){
+                path[u][v] = w;
+            }
         }
 
         for(int k=0;k<26;k++){
             for(int i=0;i<26;i++){
-                if(dist[i][k]<Integer.MAX_VALUE){
+                if(path[i][k]<Integer.MAX_VALUE){
                     for(int j=0;j<26;j++){
-                        if(dist[k][j]<Integer.MAX_VALUE){
-                            dist[i][j] = Math.min(dist[i][j],dist[i][k]+dist[k][j]);
+                        if(path[k][j]<Integer.MAX_VALUE){
+                            path[i][j] = Math.min(path[i][j],path[i][k]+path[k][j]);
                         }
+
                     }
+
                 }
             }
         }
 
-        long res = 0L;
+        long result = 0;
         for(int i=0;i<source.length();i++){
-            int c1 = source.charAt(i)-'a';
-            int c2 = target.charAt(i)-'a';
-            if(dist[c1][c2]==Integer.MAX_VALUE){
-                return -1L;
-            }else{
-                res+=(long)dist[c1][c2];
-            }
+            int s = source.charAt(i) - 'a';
+            int e = target.charAt(i) - 'a';
+            if(path[s][e]==Integer.MAX_VALUE) return -1;
+            result+=path[s][e];
         }
-
-        return res;
+        return result;
 
 
     }
