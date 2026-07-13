a54bde1485b756777c1f626c0936ecb5ab75b768
Sahil Zambre <sahildinesh.zambre2023@vitstudent.ac.in>
2026-07-13 12:11:17 +0530

    Time: 1 ms (91.63%) | Memory: 44.4 MB (99.36%) - LeetSync

--- 684-redundant-connection/redundant-connection.java
+++ 684-redundant-connection/redundant-connection.java
@@ -1,54 +1,47 @@
 class Solution {
-    int totalNodes;
     public int[] findRedundantConnection(int[][] edges) {
-        totalNodes = edges.length;
-        int res[] = new int[2];
-        DisjointSet dsu = new DisjointSet(totalNodes);
+        int n = edges.length;
+        int[] res = new int[2];
+        DisjointSet dsu = new DisjointSet(n);
         for(int edge[] : edges){
-            if(!dsu.unionBySize(edge[0]-1,edge[1]-1)){
-                res = edge;
-            }
+            if(!dsu.unionBySize(edge[0]-1,edge[1]-1)) return edge;
         }
         return res;
     }
 }
 
-class DisjointSet{
+public class DisjointSet{
     int parent[];
     int size[];
-    DisjointSet(int nodes){
-        this.parent = new int[nodes];
-        this.size = new int[nodes];
-        for(int i=0;i<nodes;i++){
-            parent[i] = i;
-            size[i] = 1;
+    DisjointSet(int node){
+        this.parent = new int[node];
+        this.size = new int[node];
+        for(int i=0;i<node;i++){
+            this.parent[i] = i;
+            this.size[i] = 1;
         }
     }
 
-    public int findRootParent(int node){
+    public int findParent(int node){
         if(node==parent[node]){
             return node;
         }
-
-        parent[node] = findRootParent(parent[node]);
+        parent[node] = findParent(parent[node]);
         return parent[node];
     }
 
     public boolean unionBySize(int node1,int node2){
-        int rootParent1 = findRootParent(node1);
-        int rootParent2 = findRootParent(node2);
-
-        if(rootParent1==rootParent2){
-            return false;
-        }
-
-        if(size[rootParent1]<size[rootParent2]){
-            parent[rootParent1] = rootParent2;
-            size[rootParent2]+=size[rootParent1];
+        int nodeParent1 = findParent(node1);
+        int nodeParent2 = findParent(node2);
+        if(nodeParent1==nodeParent2) return false;
+        if(size[nodeParent1]<size[nodeParent2]){
+            parent[nodeParent2] = nodeParent1;
+            size[nodeParent2] += size[nodeParent1];
         }else{
-            parent[rootParent2] = rootParent1;
-            size[rootParent1]+=size[rootParent2];
+            parent[nodeParent1] = nodeParent2;
+            size[nodeParent1] += size[nodeParent2];
         }
         return true;
+
     }
 }
