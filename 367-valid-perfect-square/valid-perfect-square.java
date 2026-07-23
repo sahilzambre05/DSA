c3d9f16bd974980eadf429f91516e1c0068e1046
Sahil Zambre <sahildinesh.zambre2023@vitstudent.ac.in>
2026-07-23 19:08:50 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (19.46%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        if(num==1) return true;
+        long start = 1;
+        long end = num;
+        while(start<=end){
+            long mid = start + (end-start)/2;
+            long square = mid*mid;
+
+            if(square==num){
+                return true;
+            }else if(square>num){
+                end = mid-1;
+            }else{
+                start = mid+1;
+            }
+        }
+        return false;
+    }
+}
