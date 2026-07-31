eb5b0cd6c98e1d09aa2bc70ca9eca7d0f505c423
Sahil Zambre <sahildinesh.zambre2023@vitstudent.ac.in>
2026-07-31 22:14:37 +0530

    Time: 19 ms (23.46%) | Memory: 57.2 MB (28.93%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,12 +1,35 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        int[] dp = new int[amount+1];
-        dp[0] = 1;
-        for(int i=0;i<coins.length;i++){
-            for(int j=coins[i];j<=amount;j++){
-                dp[j]+=dp[j-coins[i]];
-            }
+        int n = coins.length;
+        int dp[][] = new int[n][amount+1];
+        for(int i=0;i<n;i++){
+            Arrays.fill(dp[i],-1);
         }
-        return dp[amount];
+        int ans = recur(coins,amount,n-1,dp);
+        return ans;
+    }
+
+    public int recur(int[] coins,int amount, int index,int[][] dp){
+        if(index==0){
+            if(amount%coins[index]==0){
+                dp[index][amount] = 1;
+                return 1;
+            }
+            dp[index][amount] = 0;
+            return 0;
+        }
+
+        if(dp[index][amount]!=-1){
+            return dp[index][amount];
+        }
+
+        int pick  = 0;
+        if(amount>=coins[index]){
+            pick = recur(coins,amount-coins[index],index,dp);
+        }
+
+        int nopick = recur(coins,amount,index-1,dp);
+        dp[index][amount] = pick+nopick;
+        return dp[index][amount];
     }
 }
