5ab49f01499a5a822c07b22f02aceb7c6903ea82
Sahil Zambre <sahildinesh.zambre2023@vitstudent.ac.in>
2026-09-17 10:37:54 +0530

    Time: 3 ms (23.41%) | Memory: 64.7 MB (91.88%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,33 +1,26 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int prod = 1;
-        int zero = 0;
-        
-        int res[] = new int[nums.length];
+        int prefix[] = new int[nums.length];
+        int suffix[] = new int[nums.length];
+        Arrays.fill(prefix,1);
+        Arrays.fill(suffix,1);
+
+        // prefix[0] = nums[0];
+        // suffix[nums.length-1] = nums[nums.length-1];
+        for(int i=1;i<nums.length;i++){
+            prefix[i] = prefix[i-1]*nums[i-1]; 
+        }
+
+        for(int i=nums.length-2;i>=0;i--){
+            suffix[i] = suffix[i+1]*nums[i+1]; 
+        }
+
+        int output[] = new int[nums.length];
         for(int i=0;i<nums.length;i++){
-            if(nums[i]==0){
-                zero++;
-                
-                if(zero>=2) return res;
-                continue;
-            }
-            prod*=nums[i];
+            output[i] = prefix[i]*suffix[i];
         }
 
-        if(zero==1){
-            for(int i=0;i<nums.length;i++){
-                if(nums[i]==0){
-                    res[i] = prod;
-                    return res;
-                }
-            }
-        }
+        return output;
 
-
-        
-        for(int i=0;i<nums.length;i++){
-            res[i] = prod/nums[i];
-        }
-        return res;
     }
 }
